69b01a7f6884e14b34edf4228a4dcbf86066980c
mikehogan <me@mikehogan.net>
2015-10-08 15:48:22 +0100

    initial commit

---
+++ test/makeTreeSpec.js
@@ -0,0 +1,23 @@
+var expect = require("chai").expect;
+var mt = require("../lib/makeTree.js");
+
+describe("make tree", function(){
+    describe("#make()", function(){
+        it("should return empty tree for empty list", function(){
+            var results = mt.make([]);
+            expect(results).to.deep.equal({});
+        });
+        it("should return single new node for single begin/end pair", function(){
+            var results = mt.make([{"command":"begin","value":"nodeName"}, {"command":"end","value":"nodeName"}]);
+            expect(results).to.deep.equal({"nodeName":{}});
+        });
+        it("should return two nested nodes for nested begin/end pair", function(){
+            var results = mt.make([{"command":"begin","value":"nodeName"},{"command":"begin","value":"nestedNodeName"}, {"command":"end","value":"nestedNodeName"}, {"command":"end","value":"nodeName"}]);
+            expect(results).to.deep.equal({"nodeName":{"nestedNodeName":{}}});
+        });
+        it("should return two nested nodes with a value for nested begin/end pair and a value setter", function(){
+            var results = mt.make([{"command":"begin","value":"nodeName"},{"command":"begin","value":"nestedNodeName"}, {"command":"set","name":"someProperty","value":"someValue"}, {"command":"end","value":"nestedNodeName"}, {"command":"end","value":"nodeName"}]);
+            expect(results).to.deep.equal({"nodeName":{"nestedNodeName":{"someProperty":"someValue"}}});
+        });
+    });
+});
